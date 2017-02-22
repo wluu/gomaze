a3b4de6bbf3ae8f018b88da4265f09f2f5dd1f3f
Wilson Luu <luuwilson@gmail.com>
2017-02-21 22:39:39 -0800

    Making stubs and thinking.

--- maze.go
+++ maze.go
@@ -21,8 +21,8 @@ func main() {
 	// g.Start()
 }
 
-// track current cell
-type curcell struct {
+// x-y coordinates for the cells
+type coord struct {
 	x, y int
 }
 
@@ -38,23 +38,45 @@ func genMaze(w, h int) [][]cell {
 		maze[i] = col
 	}
 
-	/* Recursive backtracker */
-
-	// Make the initial cell the current cell and mark it as visited
-	cur := curcell{}
-	maze[cur.x][cur.y].visited = true
-
 	/*
-	2. While there are unvisited cells
-		1. If the current cell has any neighbours which have not been visited
-			1. Choose randomly one of the unvisited neighbours
-			2. Push the current cell to the stack
-			3. Remove the wall between the current cell and the chosen cell
-			4. Make the chosen cell the current cell and mark it as visited
-		2. Else if stack is not empty
-			1. Pop a cell from the stack
-			2. Make it the current cell
+		something simple: recursive backtracker
+		https://en.wikipedia.org/wiki/Maze_generation_algorithm#Depth-first_search
 	*/
 
+	// Make the initial cell the current cell and mark it as visited
+	cur := coord{}
+	maze[cur.x][cur.y].visited = true
+
+	// 2. While there are unvisited cells
+	for unvisitedCellsIn(maze) {
+		/*
+			1. If the current cell has any neighbours which have not been visited
+				1. Choose randomly one of the unvisited neighbours
+				2. Push the current cell to the stack
+				3. Remove the wall between the current cell and the chosen cell
+				4. Make the chosen cell the current cell and mark it as visited
+			2. Else if stack is not empty
+				1. Pop a cell from the stack
+				2. Make it the current cell
+		*/
+	}
+
 	return maze
+}
+
+// returns true if there are any unvisited cells in the entire maze; otherwise, returns false
+func unvisitedCellsIn(maze [][]cell) bool {
+	for _, col := range maze {
+		for _, cell := range col {
+			if !cell.visited {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+// returns a list of unvisited cells
+func unvisitedNeighbors(maze [][]cell) []string {
+	return nil
 }
